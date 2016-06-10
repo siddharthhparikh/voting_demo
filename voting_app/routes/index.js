91cec935c908b7cdfd9492e4c176da3cf620c8f5
gennarocc <gennaroccuomo@gmail.com>
2016-06-10 13:53:38 -0400

    Basic voting for one topic implemented.

--- voting_app/routes/index.js
+++ voting_app/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+// Number of votes a user starts with.
+var votes = 5;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Vote Chain' });
+});
+
+// Routing for vote topics
+router.get('/topic1', function(req, res) {
+  res.render('topic1', { title: 'Vote Chain', votes });
+});
+
+router.get('/topic2', function(req, res) {
+  res.render('topic2', { title: 'Vote Chain', votes });
+});
+
+router.get('/topic3', function(req, res) {
+  res.render('topic3', { title: 'Vote Chain', votes });
 });
 
 module.exports = router;
