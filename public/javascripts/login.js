4be5d8a39315379fc0e62de4b898683244acab1f
shparikh <shparikh@us.ibm.com>
2016-07-21 10:31:29 -0400

    syntax

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -43,26 +43,22 @@ $(document).ready(function () {
         $('#error-msg').html('Error: ' + data.status);
       }
     });
+  });
 
-    //
-    // Request to register as a new user.
-    //
-});
-
-$('#register-user').click(function(e) {
-      console.log('Sending request');
-      // Create request object.
-      var newUser = {
-        'name': $('#name').val(),
-        'email': $('#email').val(),
-        'org': $('#orginization').val()
-      };
-      //Send request object.
-      $.post('/api/register', newUser, function (data, status) {
-        if (status == 'success') {
-          $('#register-box').hide();
-          $('#error-msg').html('New account request has been sent.');
-        }
-      });
+  $('#register-user').click(function (e) {
+    console.log('Sending request');
+    // Create request object.
+    var newUser = {
+      'name': $('#name').val(),
+      'email': $('#email').val(),
+      'org': $('#orginization').val()
+    };
+    //Send request object.
+    $.post('/api/register', newUser, function (data, status) {
+      if (status == 'success') {
+        $('#register-box').hide();
+        $('#error-msg').html('New account request has been sent.');
+      }
     });
-  })
+  });
+})
