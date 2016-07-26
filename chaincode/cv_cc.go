88b5dc4b87a2dde51cebb78faf85a2c5f987ee8f
shparikh <shparikh@us.ibm.com>
2016-07-26 19:20:09 -0400

    testing

--- chaincode/cv_cc.go
+++ chaincode/cv_cc.go
@@ -993,6 +993,12 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 		return nil, err3
 	}
 	
+	username3 := []string{"Ethan3", "ecoeyta@us.ibm.com", "16"}
+	_, err3 = t.requestAccount(stub, username3)
+	if err3 != nil {
+		fmt.Println("Failed to enrolled first user")
+		return nil, err3
+	}
 	
 	fmt.Println("creating account for first user \"Ethan\"")
 	_, err4 := t.createAccount(stub, username)
