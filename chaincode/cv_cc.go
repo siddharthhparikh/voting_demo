8861962bc7848e43a952aef9faa95049a59f91a9
shparikh <shparikh@us.ibm.com>
2016-07-27 16:25:01 -0400

    testing

--- chaincode/cv_cc.go
+++ chaincode/cv_cc.go
@@ -377,7 +377,7 @@ func (t *SimpleChaincode) getOpenRequests(stub *shim.ChaincodeStub) ([]Account, 
 		fmt.Println(reflect.TypeOf(rowChan.Columns[0].GetBytes()))
 		fmt.Println(rowChan.Columns[0].GetBytes())
 		fmt.Println(string(rowChan.Columns[0].GetBytes()))
-		fmt.Println(string(rowChan.Columns[0]))
+		fmt.Println(rowChan.Columns[0])
 		fmt.Println("Appending account " + value)
 		allAccReq = append(allAccReq, account)
 		fmt.Println("All account Reqs:")
