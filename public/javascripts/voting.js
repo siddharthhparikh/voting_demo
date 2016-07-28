ffb51673adf0ead00619c6134de4b2f2d2f97443
Gennaro Cuomo <gennaroccuomo@gmail.com>
2016-07-28 11:15:43 -0400

    Changes the voting layout to a table.

--- public/javascripts/voting.js
+++ public/javascripts/voting.js
@@ -1,5 +1,6 @@
 /**
  * @author Gennaro Cuomo
+ * @author Ethan Coeytaux
  * 
  * Handels voting events including the remaining vote count.
  */
@@ -17,14 +18,13 @@ $(document).ready(function () {
   //
   // Get current topic info
   //
+  // Query the server for a the topic so that it can be loaded to the page
   $.get('/api/get-topic',{'topicID':$('#topicID').html()}, function (data, status) {
-    
+    // If there is a response.
     if(data) {
     // Create candidates
       data['choices[]'].forEach(function(entry) {
-        var candidate = '<p class="candidate">' + entry + ': </p>';
-        var voteInput = '<input type="number" class="votes" min="0" max="5"/>';
-        $('#candidates').append(candidate, voteInput);
+        $('#candidates tr:last').after('<tr><td>' + entry + '</td><td><input type="number" class="votes" min="0" max="5"/></td></tr>') 
       });
     }
   });
@@ -36,13 +36,14 @@ $(document).ready(function () {
     e.preventDefault(e);
     $.get('/api/get-topic', { "topicID": $('#topicID').html() }, function (data, status) {
       if (data) {
-        
+        // Create an array of all the votes casted.
         var votesArray = [];
         var votes = document.getElementsByClassName('votes');
         for (i = 0; i < votesArray.length; i++) {
           votesArray.push(votes[i].val);
         }
-
+        
+        // Create a vote object to submit to the server.
         var votes = {
           "topic": data.topic_id,
           "choices[]": data["choices[]"],
@@ -52,12 +53,13 @@ $(document).ready(function () {
         }
 
         console.log("VOTE: ", votes);
-
+        // Submit the vote object to the server.
         $.post('/api/vote-submit', votes, function (data, status) {
           // Handle response
           data = JSON.parse(data);
           if (data.status == 'success') {
             console.log('Votes Submitted');
+            // Reroute the user back to the home page.
             window.location.replace('/topics');
           } else {
             console.log('Error: ' + data.status);
