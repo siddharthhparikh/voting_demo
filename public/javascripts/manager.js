8caddd2baefac8ed735900db35e4a7252ccf626c
shparikh <shparikh@us.ibm.com>
2016-08-01 13:05:21 -0400

    added user check for manager webpage

--- public/javascripts/manager.js
+++ public/javascripts/manager.js
@@ -1,45 +1,48 @@
-$(document).ready(function(){
-  
-  // Populate the request table with generated buttons.
-  $.get('/api/manager', function (data, status) {
-    if(data) {
-      // Create a <tr> for each request that exists.
-      data.AllAccReq.forEach(function(entry){
-      // This was fun to write.
-         $('#request-table tr:last').after('<tr class="request"><td>' + entry.account_id + '</td><td>' + entry.email + '</td><td><input type"number" min="0" class="vote-ammount request-info" value="5"/></td><td><i class="button approve material-icons" name="' + entry.account_id + '" email="' + entry.email+ '">check</i><i class="button decline material-icons" name="' + entry.account_id + '" email="' + entry.email + '">close</i></td></tr>');
+$(document).ready(function () {
+  $(get)('/api/user', function (user) {
+    if (user.indexOf('manager') > -1) {
+      $.get('/api/manager', function (data, status) {
+        if (data) {
+          // Create a <tr> for each request that exists.
+          data.AllAccReq.forEach(function (entry) {
+            // This was fun to write.
+            $('#request-table tr:last').after('<tr class="request"><td>' + entry.account_id + '</td><td>' + entry.email + '</td><td><input type"number" min="0" class="vote-ammount request-info" value="5"/></td><td><i class="button approve material-icons" name="' + entry.account_id + '" email="' + entry.email + '">check</i><i class="button decline material-icons" name="' + entry.account_id + '" email="' + entry.email + '">close</i></td></tr>');
+          });
+        }
+      });
+
+      // Events for the approve/decline buttons.
+      $(document).on('click', '.approve', function () {
+        $(this).parent().parent().fadeOut();
+        console.log($(this).attr("name"));
+        console.log($(this).parent().parent().find('.vote-ammount').val());
+        //TODO approve the user
+        var user = {
+          ID: $(this).attr("name"),
+          VoteCount: $(this).parent().parent().find('.vote-ammount').val(),
+          Email: $(this).attr("email")
+        }
+        console.log(user);
+        $.post('/api/approved', user, function (data, status) {
+          //console.log($(this).parent().children());
+          //$(this).parent().remove();
+          location.reload();
+        });
+      });
+      $(document).on('click', '.decline', function () {
+        $(this).parent().parent().fadeOut();
+        //TODO Maybe send notification to user.
+        var user = {
+          Name: $(this).attr("name"),
+          VoteCount: $(this).parent().parent().find('.vote-ammount').val(),
+          Email: $(this).attr("email")
+        }
+        $.post('/api/declined', user, function (data, status) {
+          //$(this).parent().remove();
+          location.reload();
+        });
       });
     }
-  });
-  
-  // Events for the approve/decline buttons.
-  $(document).on('click', '.approve', function() {
-    $(this).parent().parent().fadeOut();
-    console.log($(this).attr("name"));
-    console.log($(this).parent().parent().find('.vote-ammount').val());
-    //TODO approve the user
-    var user = {
-      ID: $(this).attr("name"),
-      VoteCount: $(this).parent().parent().find('.vote-ammount').val(),
-      Email: $(this).attr("email")
-    }
-    console.log(user);
-    $.post('/api/approved', user, function (data, status) {
-      //console.log($(this).parent().children());
-      //$(this).parent().remove();
-       location.reload();
-    });
-  });
-  $(document).on('click', '.decline', function() {
-    $(this).parent().parent().fadeOut();
-    //TODO Maybe send notification to user.
-    var user = {
-      Name: $(this).attr("name"),
-      VoteCount: $(this).parent().parent().find('.vote-ammount').val(),
-      Email: $(this).attr("email")
-    }
-    $.post('/api/declined', user, function (data, status) {
-      //$(this).parent().remove();
-      location.reload();
-    });
-  });
+  })
+  // Populate the request table with generated buttons.
 });
