5db4a9ecd9491841b154de79c69bdfc379e9ee51
shparikh <shparikh@us.ibm.com>
2016-08-01 15:40:58 -0400

    syntax fix

--- public/javascripts/manager.js
+++ public/javascripts/manager.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-  $(get)('/api/user', function (user) {
-    console.log(user);
-    if (user.indexOf('manager') > -1) {
-      $.get('/api/manager', function (data, status) {
-        if (data) {
+  $.get('/api/manager', function (data, status) {
+    if (data) {
+      $(get)('/api/user', function (user) {
+        console.log(user);
+        if (user.indexOf('manager') > -1) {
           // Create a <tr> for each request that exists.
           data.AllAccReq.forEach(function (entry) {
             // This was fun to write.
@@ -11,39 +11,39 @@ $(document).ready(function () {
           });
         }
       });
-
-      // Events for the approve/decline buttons.
-      $(document).on('click', '.approve', function () {
-        $(this).parent().parent().fadeOut();
-        console.log($(this).attr("name"));
-        console.log($(this).parent().parent().find('.vote-ammount').val());
-        //TODO approve the user
-        var user = {
-          ID: $(this).attr("name"),
-          VoteCount: $(this).parent().parent().find('.vote-ammount').val(),
-          Email: $(this).attr("email")
-        }
-        console.log(user);
-        $.post('/api/approved', user, function (data, status) {
-          //console.log($(this).parent().children());
-          //$(this).parent().remove();
-          location.reload();
-        });
-      });
-      $(document).on('click', '.decline', function () {
-        $(this).parent().parent().fadeOut();
-        //TODO Maybe send notification to user.
-        var user = {
-          Name: $(this).attr("name"),
-          VoteCount: $(this).parent().parent().find('.vote-ammount').val(),
-          Email: $(this).attr("email")
-        }
-        $.post('/api/declined', user, function (data, status) {
-          //$(this).parent().remove();
-          location.reload();
-        });
-      });
     }
-  })
+  });
+
+  // Events for the approve/decline buttons.
+  $(document).on('click', '.approve', function () {
+    $(this).parent().parent().fadeOut();
+    console.log($(this).attr("name"));
+    console.log($(this).parent().parent().find('.vote-ammount').val());
+    //TODO approve the user
+    var user = {
+      ID: $(this).attr("name"),
+      VoteCount: $(this).parent().parent().find('.vote-ammount').val(),
+      Email: $(this).attr("email")
+    }
+    console.log(user);
+    $.post('/api/approved', user, function (data, status) {
+      //console.log($(this).parent().children());
+      //$(this).parent().remove();
+      location.reload();
+    });
+  });
+  $(document).on('click', '.decline', function () {
+    $(this).parent().parent().fadeOut();
+    //TODO Maybe send notification to user.
+    var user = {
+      Name: $(this).attr("name"),
+      VoteCount: $(this).parent().parent().find('.vote-ammount').val(),
+      Email: $(this).attr("email")
+    }
+    $.post('/api/declined', user, function (data, status) {
+      //$(this).parent().remove();
+      location.reload();
+    });
+  });
   // Populate the request table with generated buttons.
 });
