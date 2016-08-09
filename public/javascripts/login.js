ad8d80f44c57151dcb367049fc87a9def545af43
shparikh <shparikh@us.ibm.com>
2016-08-09 15:20:14 -0400

    testing

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -43,22 +43,20 @@ $(document).ready(function () {
   $('#submit').click(function (e) {
     e.preventDefault();
 
-    function handleFileSelect(evt) {
-      var files = evt.target.files; // FileList object
-
-      // files is a FileList of File objects. List some properties.
-      var output = [];
-      for (var i = 0, f; f = files[i]; i++) {
-        output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-          f.size, ' bytes, last modified: ',
-          f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-          '</li>');
-      }
-      document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+    var files = document.getElementById('files').files;
+    if (!files.length) {
+      alert('Please select a file!');
+      return;
     }
-    console.log(output)
-    document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
+    var reader = new FileReader();
+    reader.onloadend = function (evt) {
+      if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+        console.log(evt.target.result);
+      }
+    };
+
+    reader.readAsText(file)
 
     var user = {
       'account_id': $('#username').val(),
