8ec74db0f18526a137f30123e1a6868d6e613790
shparikh <shparikh@us.ibm.com>
2016-08-09 15:04:46 -0400

    testing

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -43,16 +43,23 @@ $(document).ready(function () {
   $('#submit').click(function (e) {
     e.preventDefault();
 
-    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp = new XMLHttpRequest();
+    function handleFileSelect(evt) {
+      var files = evt.target.files; // FileList object
+
+      // files is a FileList of File objects. List some properties.
+      var output = [];
+      for (var i = 0, f; f = files[i]; i++) {
+        output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+          f.size, ' bytes, last modified: ',
+          f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+          '</li>');
+      }
+      document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
     }
-    else {// code for IE6, IE5
-      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.open("GET", "/c/Users/IBM_ADMIN/Downloads/privKey", false);
-    xmlhttp.send();
-    xmlDoc = xmlhttp.responseText;
-    console.log(xmlDoc)
+
+    document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+    
     var user = {
       'account_id': $('#username').val(),
       'password': $('#password').val()
