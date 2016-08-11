10d8ce2c424a901082c660e56a9ef00bded9dab0
shparikh <shparikh@us.ibm.com>
2016-08-10 20:33:15 -0400

    testing

--- public/javascripts/topic-select-page.js
+++ public/javascripts/topic-select-page.js
@@ -143,8 +143,8 @@ $(document).ready(function () {
     // Voted topics will not redirect.
     if(!$(this).hasClass('voted')) {
       // Reroute the user to the topic page with a string query.
-      //console.log("context = " + context);
-      console.log("context[0] = " + context[0]);
+      console.log("context = " + $(this).context);
+      console.log("context[0] = " + $(this).context[0]);
       window.location.replace("../topic/id?=" + $(this)[0].context.id);
       window.location.replace("../topic/id?=" + $(this).context[0].id);
     } else {
