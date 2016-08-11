aeffe15ffb1c67c3ab12156ab84299af835a1320
shparikh <shparikh@us.ibm.com>
2016-08-10 22:22:55 -0400

    added option for selecting default value of account privilege

--- public/javascripts/manager.js
+++ public/javascripts/manager.js
@@ -1,13 +1,31 @@
 
 $(document).ready(function () {
   $.get('/api/manager', function (data, status) {
-    if (data.AllAccReq) {
+    if (data) {
       // Create a <tr> for each request that exists.
       data.AllAccReq.forEach(function (entry) {
         // Generate and append the new request html.
         // This was fun to write.
-        $('#request-table tr:last').after('<tr class="request"><td>' + entry.name + '</td><td>' + entry.email + '</td><td>' + entry.org + '</td></td><td>' + entry.req_time + '</td><td><select class="privilege"><option value="Default">Default</option><option value="Creator">Creator</option><option value="Manager">Manager</option></select></td><td><input type"number" min="0" class="vote-amount request-info" value="5"/></td><td><i class="button approve material-icons" name="' + entry.name + '" email="' + entry.email + '" org="' + entry.org + '" priv="">check</i><i class="button decline material-icons" name="' + entry.name + '" email="' + entry.email + '" org="' + entry.org + '"priv"">close</i></td></tr>');
+        $('#request-table tr:last').after(
+          '<tr class="request"><td>' + entry.name +
+          '</td><td>' + entry.email +
+          '</td><td>' + entry.org +
+          '</td></td><td>' + entry.req_time +
+          '</td><td><select class="privilege" name="priv">' +
+          '<option value="Default">Default</option>' +
+          '<option value="Creator">Creator</option>' +
+          '<option value="Manager">Manager</option>' +
+          '</select></td><td><input type"number" min="0" class="vote-amount request-info" value="5"/></td><td>' +
+          '<i class="button approve material-icons" name="' + entry.name +
+          '" email="' + entry.email +
+          '" org="' + entry.org +
+          '" priv="">check</i><i class="button decline material-icons" name="' + entry.name +
+          '" email="' + entry.email +
+          '" org="' + entry.org + '"priv"">close</i></td></tr>');
       });
+      $('[name=priv] option').filter(function () {
+        return ($(this).text() == entry.privileges[]); //To select Blue
+      }).prop('selected', true);
     }
   });
   // Events for the approve/decline buttons.
