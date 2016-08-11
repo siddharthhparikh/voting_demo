907b3ce39e81ac215ff92f98370acece20c3af13
shparikh <shparikh@us.ibm.com>
2016-08-10 23:46:28 -0400

    testing

--- public/javascripts/manager.js
+++ public/javascripts/manager.js
@@ -12,26 +12,28 @@ $(document).ready(function () {
           { menu: 'manager', option: 'Manager' }
         ];
         console.log(entry.privileges[0])
-        $('#request-table tr:last').after(
-          '<tr class="request"><td>' + entry.name +
-          '</td><td>' + entry.email +
-          '</td><td>' + entry.org +
-          '</td></td><td>' + entry.req_time +
-          '</td><td>' +
-          //'<select class="privilege" name="priv" value="creator">' +
-          '<select class="privilege">' +
-          //'<select required class="privilege" data-bind="kendoDropDownList: { data: menuOptions, dataTextField: "option", dataValueField: "menu", value: entry.privileges[0]} />' +
-          '<option value="default" <?php if(entry.privileges[0]=="default") echo "selected=\"selected\""; ?>>Default</option>' +
-          '<option value="creator" <?php if(entry.privileges[0]=="creator") echo "selected=\"selected\""; ?>>Creator</option>' +
-          '<option value="manager" <?php if(entry.privileges[0]=="manager") echo "selected=\"selected\""; ?>>Manager</option>' +
-          '</select>'+
-          '</td><td><input type"number" min="0" class="vote-amount request-info" value="5"/></td><td>' +
-          '<i class="button approve material-icons" name="' + entry.name +
-          '" email="' + entry.email +
-          '" org="' + entry.org +
-          '" priv="">check</i><i class="button decline material-icons" name="' + entry.name +
-          '" email="' + entry.email +
-          '" org="' + entry.org + '"priv"">close</i></td></tr>');
+          
+          $('#request-table tr:last').after(
+            '<tr class="request"><td>' + entry.name +
+            '</td><td>' + entry.email +
+            '</td><td>' + entry.org +
+            '</td></td><td>' + entry.req_time +
+            '</td><td>' +
+            //'<select class="privilege" name="priv" value="creator">' +
+            //'<?php $status = ;?>' +
+            '<select class="privilege">' +
+            //'<select required class="privilege" data-bind="kendoDropDownList: { data: menuOptions, dataTextField: "option", dataValueField: "menu", value: entry.privileges[0]} />' +
+            '<option value="default" <?php if(entry.privileges[0]=="default"){echo{"selected")};?> >Default</option>' +
+            '<option value="creator" <?php if(entry.privileges[0]=="creator"){echo{"selected")};?> >Creator</option>' +
+            '<option value="manager" <?php if(entry.privileges[0]=="manager"){echo("selected")};?> >Manager</option>' +
+            '</select>' +
+            '</td><td><input type"number" min="0" class="vote-amount request-info" value="5"/></td><td>' +
+            '<i class="button approve material-icons" name="' + entry.name +
+            '" email="' + entry.email +
+            '" org="' + entry.org +
+            '" priv="">check</i><i class="button decline material-icons" name="' + entry.name +
+            '" email="' + entry.email +
+            '" org="' + entry.org + '"priv"">close</i></td></tr>');
       });
 
     }
