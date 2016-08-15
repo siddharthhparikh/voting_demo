eca2962720368626e4ed1a3558c8633135d7cbce
shparikh <shparikh@us.ibm.com>
2016-08-15 11:27:48 -0400

    testing

--- libs/blockchainSDK.js
+++ libs/blockchainSDK.js
@@ -126,7 +126,7 @@ if (fs.existsSync("us.blockchain.ibm.com.cert")) {
             // Enroll the WebAppAdmin member with the certificate authority using
             // the one time password hard coded inside the membersrvc.yaml.
             WebAppAdmin.enroll(pwd, function (err, crypto) {
-                console.log('enrolling user \'%s\' with secret \'%s\' as registrar...', "WebAppAdmin", pw);
+                console.log('enrolling user \'%s\' with secret \'%s\' as registrar...', "WebAppAdmin", pwd);
                 if (err) return console.log('Error: failed to enroll user: %s', err);
 
                 console.log('successfully enrolled user \'%s\'!', "WebAppAdmin");
